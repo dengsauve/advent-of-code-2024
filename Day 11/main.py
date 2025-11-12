3878c318de179710e59c01aa82cae59f7423986a
Dennis Sauve <dengsauve@yahoo.com>
2025-11-11 22:07:47 -0800

    brute force ain't gonna cut it :sob:

--- Day 11/main.py
+++ Day 11/main.py
@@ -7,7 +7,7 @@ def blink(arr):
             s = arr[i]
             first_half = int(s[:len(s) // 2])
             second_half = int(s[len(s) // 2:])
-            print(first_half, second_half)
+            # print(first_half, second_half)
             arr[i] = str(first_half)
             arr.insert(i + 1, str(second_half))
             i += 1
@@ -16,10 +16,15 @@ def blink(arr):
         i += 1
     return arr
 
-array = ["125", "17"]
+# array = ["125", "17"]
+array = ["3028", "78", "973951", "5146801", "5", "0", "23533", "857"]
 
-for _ in range(25):
-    print(array)
+for _ in range(75):
+    print(f"iteration #{_}, length of array: {len(array)}")
+    # TODO: Use Unique on the array, store the difference in a temp val variable
+    # tempVal += len(array)
+    # array = array.unique()
+    # tempVal -= len(array)
     array = blink(array)
 
 
